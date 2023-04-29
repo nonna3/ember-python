9e49a560d3461d78ebd6875c4b8b3160b3b01655
Anjiang-Wei <1020681930@qq.com>
2023-04-29 11:56:58 -0700

    print levels out in the visualization script

--- settings/visualize.py
+++ settings/visualize.py
@@ -5,10 +5,14 @@ import matplotlib.pyplot as plt
 def vis(filename):
     settings = json.load(open(filename))
     plt.axvline(0, 0, 1, color='r')
+    levels = []
     for level in settings["level_settings"]:
         plt.axvline(level["adc_upper_read_ref_lvl"], 0, 1, color='r')
         plt.axvline(level["adc_lower_write_ref_lvl"], 0, 0.5, color='g', linestyle='--')
         plt.axvline(level["adc_upper_write_ref_lvl"], 0, 0.5, color='g', linestyle='--')
+        levels.append([level["adc_upper_read_ref_lvl"], level["adc_lower_write_ref_lvl"], level["adc_upper_write_ref_lvl"]])
+    print("Upper read, lower write, upper write")
+    print(levels)
     plt.xlim(-0.5, 63.5)
     plt.gca().get_yaxis().set_visible(False)
     plt.show()
